5bfd8307a623d2ea7f00fe7158d0503e704d112b
Aaron <adgholson98@gmail.com>
2023-07-31 13:31:19 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+
+const num3 = 28;
+const num4 = 6;
+const mod = (num3 % num4);
+console.log(mod);
+
+
+const set = [10, 5, 20, 15, 7];
+const max = Math.max(...set);
+console.log(max);
